922011f02b9caced9828c6db644333341b21f34c
Chetverikov Roman <r.chetverikov@securitycode.ru>
2024-03-02 17:22:47 +0300

    Add validation for input message

--- main.py
+++ main.py
@@ -11,15 +11,21 @@ bot = Bot(token=config.bot_token.get_secret_value(), session=session)
 dp = Dispatcher()
 
 USERS = {}
+EMPTY_USERNAME_ERROR_MESSAGE = "Error: Empty username."
 
 def save_user(username: str) -> None:
     if username in USERS.keys():
         return
     USERS[username] = {}
 
+def validate_message(message: types.Message):
+    if message.from_user.username is None:
+        raise RuntimeError(EMPTY_USERNAME_ERROR_MESSAGE)
+
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message):
     try:
+        validate_message(message)
         save_user(message.from_user.username)
     except Exception as ex:
         await message.answer(str(ex))
