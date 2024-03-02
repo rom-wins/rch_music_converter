1f8d376ba3cc8b5b8805850a0777cf4ccdeac0ba
Chetverikov Roman <r.chetverikov@securitycode.ru>
2024-03-02 17:17:38 +0300

    Create helloworld telegram bot

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
-from yt_music.exporter import Exporter
-from ya_music.importer import Importer
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters.command import Command
+from aiogram.client.session.aiohttp import AiohttpSession
+from config_reader import config
 
-print("Hello music converter!")
+logging.basicConfig(level=logging.INFO)
+session = AiohttpSession(proxy=config.proxy_server.get_secret_value())
+bot = Bot(token=config.bot_token.get_secret_value(), session=session)
+dp = Dispatcher()
+
+@dp.message(Command("start"))
+async def cmd_start(message: types.Message):
+    await message.answer("Register success!")
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
