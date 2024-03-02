85bb7acf481d9a842db77d7d77700654cfc9c1ff
Chetverikov Roman <r.chetverikov@securitycode.ru>
2024-03-02 17:19:43 +0300

    Add user registration

--- main.py
+++ main.py
@@ -10,8 +10,21 @@ session = AiohttpSession(proxy=config.proxy_server.get_secret_value())
 bot = Bot(token=config.bot_token.get_secret_value(), session=session)
 dp = Dispatcher()
 
+USERS = {}
+
+def save_user(username: str) -> None:
+    if username in USERS.keys():
+        return
+    USERS[username] = {}
+
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message):
+    try:
+        save_user(message.from_user.username)
+    except Exception as ex:
+        await message.answer(str(ex))
+        return
+
     await message.answer("Register success!")
 
 async def main():
