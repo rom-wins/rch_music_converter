b422e14e081813b27bb8bd4c65f3c1a760433341
romwins <chetverikov_roman@inbox.ru>
2024-03-03 17:27:08 +0200

    Add validation func for convert action

--- validation.py
+++ validation.py
@@ -2,7 +2,8 @@ from aiogram import types
 from aiogram.filters.command import CommandObject
 
 EMPTY_USERNAME_ERROR_MESSAGE = "Error: Empty username."
-EMPTY_TOKEN_ERROR_MESSAGE = "Error: no token found."
+EMPTY_TOKEN_ERROR_MESSAGE = "Error: No token found."
+EMPTY_CONVERT_URL_ERROR_MESSAGE = "Error: No url found."
 
 class Validator:
     @staticmethod
@@ -14,3 +15,8 @@ class Validator:
     def validate_add_token_command(command: CommandObject):
         if command.args is None:
             raise RuntimeError(EMPTY_TOKEN_ERROR_MESSAGE)
+
+    @staticmethod
+    def validate_convert_command(command: CommandObject):
+        if command.args is None:
+            raise RuntimeError(EMPTY_CONVERT_URL_ERROR_MESSAGE)
